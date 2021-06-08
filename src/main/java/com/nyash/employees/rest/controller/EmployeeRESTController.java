caa603d1a13fdddbcb2caec11561a2153d69ba60
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-08 08:55:09 +0700

    add changes

--- src/main/java/com/nyash/employees/rest/controller/EmployeeRESTController.java
+++ src/main/java/com/nyash/employees/rest/controller/EmployeeRESTController.java
@@ -1,12 +1,13 @@
 package com.nyash.employees.rest.controller;
 
 import com.nyash.employees.rest.entity.Employee;
+import com.nyash.employees.rest.exception_handling.EmployeeIncorrectData;
+import com.nyash.employees.rest.exception_handling.NoSuchEmployeeException;
 import com.nyash.employees.rest.services.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,6 +31,29 @@ public class EmployeeRESTController {
     @GetMapping("/employees/{id}")
     public Employee getEmployee(@PathVariable("id") int id){
         Employee employee = employeeService.getEmployee(id);
+
+        if (employee == null){
+            throw new NoSuchEmployeeException("There is no employee with ID = " +
+                     id + " in Database");
+        }
         return employee;
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            NoSuchEmployeeException exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler
+    public ResponseEntity<EmployeeIncorrectData> handleException(
+            Exception exception){
+        EmployeeIncorrectData data = new EmployeeIncorrectData();
+        data.setInfo(exception.getMessage());
+
+        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
     }
 }
