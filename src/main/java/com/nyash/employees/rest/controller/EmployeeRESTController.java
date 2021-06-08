1da851dd99ffb9f88275cf7ad1a77cd095574144
Nyaashmyaash <81423982+Nyaashmyaash@users.noreply.github.com>
2021-06-09 01:20:31 +0700

    add changes

--- src/main/java/com/nyash/employees/rest/controller/EmployeeRESTController.java
+++ src/main/java/com/nyash/employees/rest/controller/EmployeeRESTController.java
@@ -38,22 +38,4 @@ public class EmployeeRESTController {
         }
         return employee;
     }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            NoSuchEmployeeException exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(
-            Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 }
